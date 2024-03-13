90f9d50eb4cbd140dd4ac6fd37418c5e71b62b46
Lakshay Kumar Kashyap <131148878+LakshayKumar9347@users.noreply.github.com>
2024-03-13 12:55:18 +0530

    monolithic architecture

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config();
 
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true } 
+}
 
 module.exports = nextConfig
